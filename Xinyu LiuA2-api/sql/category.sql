cf13eb714d0e35f9ce3b7f8c9d3fa98c05affb2c
EdithGX811 <2774587455@qq.com>
2024-10-02 15:45:33 +0800

    commit 13
    Upload the database and move it to the back-end folder for easy assignment submission

---
+++ Xinyu LiuA2-api/sql/category.sql
@@ -0,0 +1,3 @@
+INSERT INTO `category` (`CATEGORY_ID`, `NAME`) VALUES (1, 'Education');
+INSERT INTO `category` (`CATEGORY_ID`, `NAME`) VALUES (2, 'Health');
+INSERT INTO `category` (`CATEGORY_ID`, `NAME`) VALUES (3, 'Environment');
